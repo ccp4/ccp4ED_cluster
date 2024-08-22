6e4809bb438bc9b185062b3567173aee6f8d0407
tarik <ronandrevon@gmail.com>
2024-08-22 15:39:36 +0100

    bug fixes

--- scripts/felix_installer.sh
+++ scripts/felix_installer.sh
@@ -1,7 +1,16 @@
-pdir=`dirname $0`
 
+#### fftw
+if [ ! -d Documents/fftw ]; then mkdir -p Documents/fftw;fi
+cd Documents/fftw
+wget http://www.fftw.org/fftw-3.3.9.tar.gz
+tar -xvzf fftw-3.3.9.tar.gz
+cd fftw-3.3.9
+./configure --enable-shared # --enable-float --enable-threads
+make
+sudo make install
 
 ### compile native FELIX
+cd /home/$USER
 mkdir -p ~/Documents/git/ccp4/src/
 export PATH=/usr/lib/openmpi/bin/
 cd ~/Documents/git/ccp4/src/
@@ -11,4 +20,4 @@ git checkout v2.2.0
 cd src
 make
 
-sudo ln -s /home/$USER/Documents/git/ccp4/src/felix/src/felix.OPT64NGNU.d /home/$USER/felix
+ln -s /home/$USER/Documents/git/ccp4/src/felix/src/felix.OPT64NGNU.d /home/$USER/felix
--- scripts/fftw_installer.sh
+++ scripts/fftw_installer.sh
@@ -1,10 +1,2 @@
-pdir=`dirname $0`
-#### fftw
-if [ ! -d Documents/fftw]; then mkdir -p Documents/fftw;fi
-cd Documents/fftw
-wget http://www.fftw.org/fftw-3.3.9.tar.gz
-tar -xvzf fftw-3.3.9.tar.gz
-cd fftw-3.3.9
-./configure --enable-shared # --enable-float --enable-threads
-make
+cd Documents/fftw/fftw-3.3.9
 sudo make install
